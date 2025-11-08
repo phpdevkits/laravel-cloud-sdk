cf3fa1d39e94cd7e03109d808a8b0d76cb762435
Francisco Barrento <francisco.barrento@gmail.com>
2025-11-08 19:19:57 +0000

    fix: update test configurations for PEST compatibility and enhance Rector settings

--- rector.php
+++ rector.php
@@ -3,22 +3,18 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/src',
         __DIR__.'/tests',
     ])
-    ->withSkip([
-        AddOverrideAttributeToOverriddenMethodsRector::class,
-    ])
     ->withPreparedSets(
         deadCode: true,
         codeQuality: true,
+        codingStyle: true,
         typeDeclarations: true,
         privatization: true,
         earlyReturn: true,
-        strictBooleans: true,
     )
     ->withPhpSets();
